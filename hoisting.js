49ea81f49cdea1b3991a311c57f248d33c2a3af2
Robert Howden <robert.howden@current-rms.com>
2020-04-20 15:43:25 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -25,10 +25,13 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
+
 
   function sayMy() {
-    console.log(name);
+
     var name = "Kristin";
+    console.log(name);
   }
+
+  sayMy();
 }
